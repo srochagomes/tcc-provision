a0e8c1e30ca6d7bf6fd6461d309667d8b5e734a3
tcc_puc_ls <srochg@gmail.com>
2021-02-22 19:38:51 -0300

    Adicionando apigateway e auth

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -3,6 +3,10 @@ kubectl apply -f istio/2-istio-minikube.yaml
 kubectl apply -f istio/3-kiali-secret.yaml
 kubectl apply -f istio/4-label-default-namespace.yaml
 
+kubectl apply -f app/tcc-auth-api-deployment.yaml
+kubectl apply -f app/tcc-auth-api-service.yaml
+kubectl apply -f app/tcc-auth-api-hpa.yaml
+
 kubectl apply -f app/tcc-db-configmap.yaml
 kubectl apply -f app/tcc-db-pv.yaml
 kubectl apply -f app/tcc-db-deployment.yaml
@@ -13,4 +17,8 @@ kubectl apply -f app/tcc-front-hpa.yaml
 kubectl apply -f app/tcc-proc-ind-api-deployment.yaml
 kubectl apply -f app/tcc-proc-ind-api-service.yaml
 kubectl apply -f app/tcc-proc-ind-api-hpa.yaml
+kubectl apply -f app/tcc-router-api-deployment.yaml
+kubectl apply -f app/tcc-router-api-service.yaml
+kubectl apply -f app/tcc-router-api-hpa.yaml
+
 kubectl apply -f tcc-ms-route-rules.yaml
